9381a7097476c08e04d0c9f84e4f9a2d6b34f5b3
yanen <35618977+yanen@users.noreply.github.com>
2020-02-08 18:38:01 +0800

    Updates to UI
    Rounded buttons and bolded titles added. Added some padding to the toggle buttons so they do not stick to the sides of the windows

--- Survey Scan Beta (Table Testing)/Table test.py
+++ Survey Scan Beta (Table Testing)/Table test.py
@@ -12,9 +12,11 @@ from kivy.properties import StringProperty
 Window.size = (1000, 800)
 #Window.clearcolor = (1, 1, 1, 1)
 toggle_states = [["normal" for i in range(4)] for i in range(10)]
+default_down_states = [0,1,2,3,0,1,2,3,0,1]
+for i in range(len(default_down_states)):
+    toggle_states[i][default_down_states[i]] = "down"
 class Item(GridLayout):
     def adjust_data(self, rvRow,button_index,state,refff):
-        #print(rvRow,button_index,state)
         toggle_states[int(rvRow)] = ["normal" for i in range(4)]
         toggle_states[int(rvRow)][int(button_index)] = str(state)
         root = App.get_running_app().root
@@ -23,8 +25,7 @@ class Item(GridLayout):
         refff.b2state = refff.ids.b2.state
         refff.b3state = refff.ids.b3.state
         refff.b4state = refff.ids.b4.state
-        #root.ids.rv.refresh_from_data()
-        pass
+        
 kv = """
 
 <StockList>: # RecycleView
@@ -47,13 +48,16 @@ kv = """
     b2state: 'normal'
     b3state: 'normal'
     b4state: 'normal'
-    
     cols: 1
     Label:
-        text: root.name
+        markup: True
+        text: "[size=30]" + root.name
     BoxLayout:
         size_hint_y: None
+        size_hint_x: .8
         height: "40dp"
+        FloatLayout:
+            size_hint: 0.1, 0.1
         ToggleButton:
             text:"Multiple-Choice"
             id: b1
@@ -84,10 +88,46 @@ kv = """
             state:root.b4state
             on_release:root.adjust_data(root.row_count,3,self.state,root)
             allow_no_selection:False
+        FloatLayout:
+            size_hint: 0.1, 0.1
         
 BoxLayout:
+
+    orientation:"vertical"
+    size_hint:(1, 0.97)
+    BoxLayout:
+        size_hint:(0.95, None)
+        pos_hint: {'center_x': .5, 'center_y': .5}
+        orientation:"horizontal"
+        RoundedButton:
+            size_hint:(0.8, None)
+            text:"Back"
+            
+        FloatLayout:
+        
+        Label:
+            markup: True
+            text:"[size=45][b]Question Overview[/b]"
+            
+        FloatLayout:
+        
+        RoundedButton:
+            size_hint:(0.8, None)
+            text:"Export data"
+            
+        
     StockList:
         id:rv
+
+<RoundedButton@Button>:
+    background_color: 0,0,0,0  # the last zero is the critical on, make invisible
+    canvas.before:
+        Color:
+            rgba: (.4,.4,.4,1) if self.state=='normal' else (0,.7,.7,1)  # visual feedback of press
+        RoundedRectangle:
+            pos: self.pos
+            size: self.size
+            radius: [50,]
 
 """
 
@@ -96,8 +136,9 @@ BoxLayout:
 class StockList(RecycleView):
     
     def getData(self):
+        number_of_elements = 10
         data = []
-        for i in range(0,10):
+        for i in range(number_of_elements):
             add = {}
             add['name'] = 'item ' + str(i)
             add['b1state'] = toggle_states[i][0]
@@ -106,8 +147,6 @@ class StockList(RecycleView):
             add['b4state'] = toggle_states[i][3]
             add['row_count'] = str(i)
             data.append(add)
-        #print()
-        #print(data)
         return data
 
 
@@ -118,5 +157,4 @@ class TestApp(App):
 
 if __name__ == '__main__':
   TestApp().run()
-
 
