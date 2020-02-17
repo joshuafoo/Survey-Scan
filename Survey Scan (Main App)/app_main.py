48c1b2a31173fe5cf6372bf6e23263e541d9f393
yanen <35618977+yanen@users.noreply.github.com>
2020-02-17 18:33:38 +0800

    Major Ui update
    1. Integrated third screen into main script and main.kv file.
    2. Commented out import pandas and math and text blob as it is not needed yet and will complicate things.
    Note: This file will require Mathplotlib and Kivy garden(Mathplotlib backend) to run , import line is on line 32 otherwise everything will not work.

--- Survey Scan (Main App)/app_main.py
+++ Survey Scan (Main App)/app_main.py
@@ -1,12 +1,16 @@
 #--------Import all required packages--------#
 try:
     import kivy
-    import pandas as pd
-    import math
+    #import pandas as pd
+    #import math
+
+    import matplotlib.pyplot as plt
+    import numpy as np
 except ImportError:
     raise ImportError("Some Packages were not installed properly. Please install them and try again")
 
-from textblob import TextBlob
+from functools import partial
+#from textblob import TextBlob
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, NoTransition
 from kivy.config import Config
@@ -22,6 +26,13 @@ from kivy.uix.popup import Popup
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.recycleview import RecycleView
 from kivy.lang import Builder
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+from kivy.properties import StringProperty
+from kivy.uix.togglebutton import ToggleButton
+
 
 class First(Screen):
     def on_enter(self):Window.size = (500, 100)
@@ -138,9 +149,217 @@ class Second(Screen):
 
 
 class Third(Screen):
+    def save(self):#save button pressed
+        print("Save to file enter logic here")
     def on_enter(self):
         Window.size = (1000, 800)#set size of window after transtion to this screen
 
+class MyLayout():pass
+
+class MyPopup(Popup):pass
+
+class Questionlist(RecycleView):
+    number_of_elements = 10
+    def getData(self):
+        data=[]
+        
+        for i in range(self.number_of_elements):
+            add = {}
+            add['name'] = 'Question ' + str(i)
+            add['row_count'] = str(i)
+            data.append(add)
+            
+        return data
+class Item2(FloatLayout):
+
+    def __init__(self, **kwargs):
+        super(Item2, self).__init__(**kwargs)
+        #define variables
+        self.array = ["Statistics","Pie Chart","Bar Chart","Anomalies"]
+        self.current_segement = globals()[self.array[0].replace(" ", "_")]()
+
+    def fire_popup(self,name):
+        #--------Creates popup widget--------#
+        self.current_segement = globals()[self.array[0].replace(" ", "_")]()
+        pops = MyPopup()
+        
+        if(len(name)>105):#check if name is more than 2 lines
+            pops.title = name[:105] + "..."#Truncate name
+        else:
+            pops.title = name
+            
+        pops.title_size = 40
+
+        #--------Create ui elements--------#
+
+        # creating Floatlayout
+        Fl = FloatLayout()
+        Bl = BoxLayout(size_hint_y= None,
+                       size_hint_x= .95,
+                       height= "30dp",
+                       pos_hint = {'center_y': .95, 'center_x': .5})
+
+        #Toggle button events
+        def change_type(name,foo):
+            #unloads previous tab and load new tab
+            plt.close()#close mathplot figure
+            Fl.remove_widget(self.current_segement)
+            self.current_segement = globals()[name.replace(" ", "_")]()
+            Fl.add_widget(self.current_segement)
+            
+            Fl.remove_widget(btn)
+            Fl.add_widget(btn)
+            print(name + " selected")#debug
+
+        #setup toggle buttons
+        
+        toggle_arra = []
+        for i in self.array:
+            Tb = ToggleButton(text=i,
+                              group = "1",
+                              state = "normal",
+                              allow_no_selection = False)
+
+            Tb.bind(on_release = partial(change_type,i))
+            toggle_arra.append(Tb)
+            Bl.add_widget(Tb)
+
+        toggle_arra[0].state = "down"#set first toggle button to be default selected
+        
+        
+        #"Dismiss" button
+        btn = Button(text ='Dismiss',
+                     size_hint =(.2, .1),
+                     background_color =(.3, .6, .7, 1),
+                     pos_hint ={'center_y': 0.05, 'center_x': .5},
+                     on_press = lambda *args: pops.dismiss())
+        
+
+
+        #--------Add widgets to popup--------#
+
+        Fl.add_widget(self.current_segement)
+        Fl.add_widget(Bl)
+        Fl.add_widget(btn)
+        
+        pops.add_widget(Fl)
+        pops.open()
+
+class Anomalies(StackLayout):
+    def __init__(self, **kwargs):
+        super(Anomalies, self).__init__(**kwargs)
+        #--------Set constraints of view--------#        
+        self.orientation = "tb-lr"#left right top bottom
+        self.size_hint = (0.9, 0.8)
+        self.pos_hint ={'center_y': 0.5, 'center_x': 0.5}
+
+#--------create scrollable label--------#
+        long_text = """There is a lot of data anomalies.There is something wong."""
+        
+        l = ScrollableLabel(text=long_text)
+
+        #add scrollable label to self
+        self.add_widget(l)
+        
+        
+class Pie_Chart(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Pie_Chart, self).__init__(**kwargs)
+        self.size_hint = (0.9, 0.9)
+        self.pos_hint ={'center_y': 0.45, 'center_x': 0.5}
+
+#--------Create pie chart--------#
+        
+        plt.clf()#clear all
+        plt.rcParams['font.size'] = 25.0#set font size of words
+        fig, ax = plt.subplots(figsize=(8, 5), subplot_kw=dict(aspect="equal"))
+
+        recipe = ["375 g flour",
+                  "75 g sugar",
+                  "250 g butter",
+                  "300 g berries"]#data
+
+        data = [float(x.split()[0]) for x in recipe]
+        ingredients = [x.split()[-1] for x in recipe]
+
+
+        def func(pct, allvals):
+            absolute = int(pct/100.*np.sum(allvals))
+            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+
+
+        wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
+                                          textprops=dict(color="w"))
+
+        ax.legend(wedges, ingredients,
+                  title="Ingredients",
+                  loc="center left",
+                  bbox_to_anchor=(1, 0, 0.5, 1))
+
+        plt.setp(autotexts, size=20, weight="bold")
+
+        ax.set_title("Matplotlib bakery: A pie")
+        ax.autoscale(enable=True)
+
+#--------Add pie chart to self--------#
+        
+        self.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+class Bar_Chart(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Bar_Chart, self).__init__(**kwargs)
+#--------Set constraints of view--------#
+        self.size_hint = (0.8, 0.8)
+        self.pos_hint ={'center_y': 0.503, 'center_x': 0.5}#left right top bottom
+
+#--------Create graph--------#
+        
+        plt.clf()#clear all
+        objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+        y_pos = np.arange(len(objects))
+        performance = [10,8,6,4,2,1]
+
+        plt.bar(y_pos, performance, align='center', alpha=0.5)
+        plt.xticks(y_pos, objects)
+        plt.ylabel('Usage')
+        plt.title('Programming language usage')
+
+#--------Add graph to self--------#
+        
+        self.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+class ScrollableLabel(ScrollView):
+    text = StringProperty('')
+    
+class Statistics(StackLayout):
+    def __init__(self, **kwargs):
+        super(Statistics, self).__init__(**kwargs)
+#--------Set constraints of view--------#        
+        self.orientation = "tb-lr"#left right top bottom
+        self.size_hint = (0.9, 0.8)
+        self.pos_hint ={'center_y': 0.5, 'center_x': 0.5}
+
+#--------create scrollable label--------#
+        long_text = """Mean/Average: 100000
+Mode: 10000
+Median: 10000
+Standard Deviation: 100000000000
+Interquartile Range: 10000000000
+Upper Quartile: 10000000000
+Lower Quartile: 10000000000
+Total Responses: 1000000000000000000000000000000000000000"""
+        
+        l = ScrollableLabel(text=long_text)
+
+        #add scrollable label to self
+        self.add_widget(l)
+        
+    
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': "Button " + str(x+1),"row":str(x)} for x in range(30)]
 
 # Scene manager class
 class MyApp(App):
@@ -164,3 +383,4 @@ class Error(Exception):
 # run the App
 if __name__ == "__main__":
     MyApp().run()
+
